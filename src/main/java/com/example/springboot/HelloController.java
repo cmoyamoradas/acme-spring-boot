445b1b2a0ead95b92445f54731439b889c010db5
Carlos Moya Moradas <cmmoradas@gmail.com>
2024-09-19 16:07:32 +0200

    Fixing

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -9,10 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 	public static final String login = "AKIAJXBOVX5Q2EULDUIA";
-	public static final String pwd ="SqcyDpetv+pCsbNYWHDLE8yR5mJ13MI+4d8NOwtM";
-	@PostMapping ("/greeting")
+	@PostMapping ("/greeting")
 	public String greetingSubmit(@ModelAttribute Greeting greeting, Model model){
 		model.addAttribute("greeting", greeting);
-		return "Hello, I'm a vulnerability";
+		return "Hello";
 	}
 }
