9e77103bd050c8a1f80b38ab14d9de225cb8f83f
Carlos Moya Moradas <cmmoradas@gmail.com>
2024-09-19 14:40:50 +0200

    adding applicable vuln

---
+++ src/main/java/com/example/springboot/Greeting.java
@@ -0,0 +1,10 @@
+public class Greeting {
+    private long id;
+    private String content;
+    public String getContent(){
+        return content;
+    }
+    public long getId(){
+        return id;
+    }
+}
